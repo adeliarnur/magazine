b26f14442f4cdbd928735b4ee7556cc23c2eecb0
cendra devayana putra <cendradevayanaputra@gmail.com>
2019-05-12 18:01:33 +0700

    Penyakit + CT

---
+++ Admin_Penyakit_Delete_action.php
@@ -0,0 +1,21 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "psi";
+
+$connection = mysqli_connect($servername, $username, $password, $dbname);
+if (!$connection){
+
+	die("Connection Failed:".mysqli_connect_error());
+}
+
+
+
+
+
+$id = $_GET['id'];
+mysqli_query($connection,"DELETE FROM tabel_penyakit WHERE id='$id'")or die(mysql_error());
+
+header("location:admin_penyakit.php?pesan=delete");
+?>
