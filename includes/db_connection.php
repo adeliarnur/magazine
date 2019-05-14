39a2fa7bdcc890b275a4359740793a29b9e83641
Syakir Ali <email02081997@gmail.com>
2019-05-15 05:36:33 +0700

    update db

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,8 +1,8 @@
 <?php
-$dbhost='sql12.freemysqlhosting.net';
-$dbuser='sql12290730';
-$dbpass='ynCnGuDI4V';
-$dbname='sql12290730';
+$dbhost='alibobo.tech';
+$dbuser='psi_kel_2';
+$dbpass='SI@irlangg@';
+$dbname='psi_kel_2';
 $db=new mysqli($dbhost,$dbuser,$dbpass,$dbname);
 if ($db->connect_error) {
     die("Connection failed: " . $db->connect_error);
