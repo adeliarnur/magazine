3574f8ff8f497c238c682a9a009f4c65a513de44
Unknown <nululalbab@gmail.com>
2019-05-21 23:01:52 +0700

    Fixing Delete Status

--- hapus-status.php
+++ hapus-status.php
@@ -2,7 +2,7 @@
 include "includes/db_connection.php";
 $id = $_GET['id'];
 
-mysqli_query($db,"DELETE FROM status_bencana WHERE id_status_bencana='$id'")or die("Gagal delete");
+mysqli_query($db,"DELETE FROM status_bencana WHERE id_status_bencana='$id'")or die("Gagal delete database");
 
 header("location:AdminStatusBencana.php?pesan=delete");
 ?>
