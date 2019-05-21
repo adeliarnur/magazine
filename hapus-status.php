8816e50864971b5e15e6b664aba53a3e8ad5acbc
Unknown <nululalbab@gmail.com>
2019-05-21 23:01:16 +0700

    Update Delete Status Bencana

---
+++ hapus-status.php
@@ -0,0 +1,8 @@
+<?php
+include "includes/db_connection.php";
+$id = $_GET['id'];
+
+mysqli_query($db,"DELETE FROM status_bencana WHERE id_status_bencana='$id'")or die("Gagal delete");
+
+header("location:AdminStatusBencana.php?pesan=delete");
+?>
