647b40005f98fbd5f8be1f9fc9581e9c186a2633
Ali <email02081997@gmail.com>
2019-05-28 15:35:47 +0700

    prabencana fix thumbnail

--- AdminMinimalisirDampakBencana.php
+++ AdminMinimalisirDampakBencana.php
@@ -1,14 +1,15 @@
 <?php
 include "includes/db_connection.php";
-
+global $konten, $id_kategori;
+// echo empty($_FILES['thumbnail']['name'])?"tak ada":"ada";
+// die();
 if ($_GET['edit']) {
-	global $konten;
 	$id_kategori = intval($_GET['edit']);
 	$sql = "SELECT * FROM `pra-bencana_konten`
 					WHERE id=$id_kategori";
 
 	if ($result = $db->query($sql)) {
-	    $konten = $result->fetch_array();
+	    $konten = $result->fetch_object();
 	} else {
 		$konten = false;
 	}
@@ -16,50 +17,75 @@ if ($_GET['edit']) {
 
 if (count($_POST) > 0){
 	try {
+		global $konten;
 		$judul = $_POST['judul'];
 		$kategori = intval($_POST['kategori']);
 		$konten = $_POST['konten'];
 
 		$upload_image = true;
-		$image = $_FILES['thumbnail']['name'];
+		if (!empty($_FILES['thumbnail']['name'])) {
+			$image = $_FILES['thumbnail']['name'];
 
-		// image file directory
-		$target = "img/prabencana_thumbnail/" . basename($image);
+			// image file directory
+			$target = "img/prabencana_thumbnail/" . basename($image);
 
-		// check image
-		if (getimagesize($_FILES['thumbnail']["tmp_name"]) === false) {
+			// check image
+			if (getimagesize($_FILES['thumbnail']["tmp_name"]) === false) {
+				$upload_image = false;
+			}
+
+			// check image
+			$allowed_ext = ['jpg', 'png', 'bmp', 'jpeg'];
+			// print_r(end(explode('.', basename($image))));
+			// die();
+			$ext = explode('.', basename($image));
+			$ext = end($ext);
+			$ext = strtolower($ext);
+			if(!in_array($ext, $allowed_ext)){
+				$upload_image = false;
+			}
+
+			// uploading image
+			if (!move_uploaded_file($_FILES['thumbnail']['tmp_name'], $target)) {
+				$upload_image = false;
+			}
+
+			if (!$upload_image) {
+				echo "gagal mengupload thumbnail";
+				die();
+			}
+		} else {
 			$upload_image = false;
 		}
 
-		// check image
-		$allowed_ext = ['jpg', 'png', 'bmp', 'jpeg'];
-		// print_r(basename($image));
-		// die();
-		$ext = end(explode('.', basename($image)));
-		$ext = strtolower($ext);
-		if(!in_array($ext, $allowed_ext)){
-			$upload_image = false;
+		if ($_GET['edit']) {
+			if (!$upload_image) {
+				$sql = "UPDATE `pra-bencana_konten` SET `judul`=?, `konten`=?, `kategori`=? WHERE `id`=?";
+				$stmt = $db->prepare($sql);
+				$stmt->bind_param("ssii", $judul, $konten, $kategori, $id_kategori);
+			} else {
+				// global $konten;
+				$sql = "UPDATE `pra-bencana_konten` SET `judul`=?, `konten`=?, `kategori`=?, `thumbnail`=? WHERE `id`=?";
+				$stmt = $db->prepare($sql);
+				$stmt->bind_param("ssisi", $judul, $konten, $kategori, $target, $id_kategori);
+			}
+		} else {
+			if ($upload_image) {
+				$sql = "INSERT INTO `pra-bencana_konten` (`judul`, `konten`, `kategori`, `thumbnail`)
+						VALUES (?, ?, ?, ?)";
+
+				$stmt = $db->prepare($sql);
+				$stmt->bind_param("ssis", $judul, $konten, $kategori, $target);
+			} else {
+				$sql = "INSERT INTO `pra-bencana_konten` (`judul`, `konten`, `kategori`)
+						VALUES (?, ?, ?)";
+
+				$stmt = $db->prepare($sql);
+				$stmt->bind_param("ssis", $judul, $konten, $kategori);
+			}
+
+			// $konten = addslashes($_POST['konten']);
 		}
-
-		// uploading image
-		if (!move_uploaded_file($_FILES['thumbnail']['tmp_name'], $target)) {
-			$upload_image = false;
-		}
-
-		if (!$upload_image) {
-			echo "gagal mengupload thumbnail";
-			die();
-		}
-
-		$thumbnail = $target;
-
-		// $konten = addslashes($_POST['konten']);
-
-		$sql = "INSERT INTO `pra-bencana_konten` (`judul`, `konten`, `kategori`, `thumbnail`)
-				VALUES (?, ?, ?, ?)";
-
-		$stmt = $db->prepare($sql);
-		$stmt->bind_param("ssis", $judul, $konten, $kategori, $thumbnail);
 
 		if ($stmt->execute() === FALSE) {
 			echo "Error: " . $sql . "<br>" . $db->error;
@@ -74,11 +100,12 @@ if (count($_POST) > 0){
 }
 
 function tambahJS() {
+	global $konten;
 	?>
 	<script src="summernote/summernote-bs4.min.js"></script>
 	<script type="text/javascript">
 		$(document).ready(function() {
-			var markupStr = "<?php if($konten) echo $konten['konten']; ?>";
+			var markupStr = "<?php echo $konten? $konten->konten:""; ?>";
 			// $('#summernote').summernote('code', markupStr);
 			$('#summernote').summernote({
 				placeholder: 'Masukkan konten di sini',
@@ -120,15 +147,15 @@ include "includes/header-admin.php";
 		<form id="konten-baru" method="post" enctype="multipart/form-data">
 			<div class="form-group">
 				<label for="inputJudul">Judul</label>
-				<input type="text" class="form-control" id="inputJudul" name="judul" placeholder="Masukkan Judul">
+				<input type="text" class="form-control" id="inputJudul" name="judul" placeholder="Masukkan Judul" value="<?php echo $konten?$konten->judul:"";?>">
 				</div>
 
 				<div class="form-group">
 					<label for="inputJudul">Kategori</label>
 					<select class="form-control" id="inputKategori" name="kategori">
-						<option value="1">Persiapan Diri</option>
-						<option value="2">Persiapan Di Dalam Rumah</option>
-						<option value="3">Persiapan Di Luar Rumah</option>
+						<option value="1"<?php echo $konten?($konten->kategori == "1"?" selected":""):""?>>Persiapan Diri</option>
+						<option value="2"<?php echo $konten?($konten->kategori == "2"?" selected":""):""?>>Persiapan Di Dalam Rumah</option>
+						<option value="3"<?php echo $konten?($konten->kategori == "3"?" selected":""):""?>>Persiapan Di Luar Rumah</option>
 					</select>
 				</div>
 
@@ -138,8 +165,8 @@ include "includes/header-admin.php";
 				</div>
 
 			<div class="form-group">
-				<textarea id="summernote" class="form-control"></textarea>
-				<input type="hidden" id="summernote_konten" name="konten">
+				<textarea id="summernote" class="form-control"><?php echo $konten?$konten->konten:""?></textarea>
+				<input type="hidden" id="summernote_konten" name="konten" value="<?php echo $konten?$konten->konten:""?>">
 			</div>
 			<input class="btn btn-sm btn-primary" type="submit" name="submit" value="Submit">
 		</form>
